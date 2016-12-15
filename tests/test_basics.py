3407529b3ae04489d41b0bd635565bd63f141ec2
Vincent Aranega <vincent.aranega@axellience.com>
2016-12-15 11:02:49 +0100

    Upgrade existing basic tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,13 +1,17 @@
 import pytest
-import pyecore.ecore as Ecore
+from pyecore.ecore import *
 
 def test_ecore_URI():
-    assert Ecore.nsURI == 'http://www.eclipse.org/emf/2002/Ecore'
+    assert nsURI == 'http://www.eclipse.org/emf/2002/Ecore'
 
 
 def test_get_existing_EClassifier():
-    assert Ecore.getEClassifier('EClass')
+    assert getEClassifier('EClass')
 
 
 def test_get_nonexisting_EClassifier():
-    assert not Ecore.getEClassifier('EEClass')
+    assert not getEClassifier('EEClass')
+
+
+def test_ecore_isinstance_none():
+    assert EcoreUtils.isinstance(None, EClass)
