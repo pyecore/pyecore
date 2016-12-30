c462089ba5e17a6c7784a809bdf0f4264a79e160
Vincent Aranega <vincent.aranega@gmail.com>
2016-12-30 12:27:55 +0100

    Update the Ecore/UML test using ResourceSet
    In this example, the UML.ecore is straight from the Eclipse
    version. References are made to 'plateform:/plugin...' stuffs which
    are related to Eclipse EMF plugin registered in memory. To deal with
    this, this version registered in the ResourceSet (could also be done
    in the global_registry) the missing resources.

--- xmi_experiment.py
+++ xmi_experiment.py
@@ -21,33 +21,21 @@ def _build_path(obj):
 
 
 if __name__ == '__main__':
-    # # UMLPrimitiveTypes Creation
-    # umltypes = Ecore.EPackage('umltypes')
-    # String = Ecore.EDataType('String', str)
-    # Boolean = Ecore.EDataType('Boolean', bool, False)
-    # Integer = Ecore.EDataType('Integer', int, 0)
-    # UnlimitedNatural = Ecore.EDataType('UnlimitedNatural', int, 0)
-    # Real = Ecore.EDataType('Real', float, 0.0)
-    # umltypes.eClassifiers.extend([String, Boolean, Integer, UnlimitedNatural, Real])
-    # global_registry['platform:/plugin/org.eclipse.uml2.types/model/Types.ecore'] = umltypes
-    #
-    # resource = XMIResource(URI('xmi-tests/Ecore.ecore'))
-    # resource.load()
-    # root = resource.contents[0]
-    # global_registry['platform:/plugin/org.eclipse.emf.ecore/model/Ecore.ecore'] = root
-    #
-    # resource = XMIResource(URI('xmi-tests/UML.ecore'))
-    # resource.load()
-    # root = resource.contents[0]
-    # Class = root.getEClassifier('Class')
-    # Property = root.getEClassifier('Property')
-    # Visibility = root.getEClassifier('VisibilityKind')
-    # c = Class()
-    # p = Property()
-    # p.name = 'testing'
-    # p.type = c
-    # c.ownedAttribute.append(p)
-    # assert p in c.ownedAttribute
     rset = ResourceSet()
+    rset.metamodel_registry.update(global_registry)
+
+    # UMLPrimitiveTypes Creation
+    umltypes = Ecore.EPackage('umltypes')
+    String = Ecore.EDataType('String', str)
+    Boolean = Ecore.EDataType('Boolean', bool, False)
+    Integer = Ecore.EDataType('Integer', int, 0)
+    UnlimitedNatural = Ecore.EDataType('UnlimitedNatural', int, 0)
+    Real = Ecore.EDataType('Real', float, 0.0)
+    umltypes.eClassifiers.extend([String, Boolean, Integer, UnlimitedNatural, Real])
+    rset.resources['platform:/plugin/org.eclipse.uml2.types/model/Types.ecore'] = umltypes
+
+    # Register Ecore metamodel instance
     resource = rset.get_resource(URI('xmi-tests/Ecore.ecore'))
-    print(resource.contents)
+    rset.resources['platform:/plugin/org.eclipse.emf.ecore/model/Ecore.ecore'] = resource.contents[0]
+
+    resource = rset.get_resource(URI('xmi-tests/UML.ecore'))
