c328f8298b00e493d040cbba7db88b04dc9dad91
Vincent Aranega <vincent.aranega@gmail.com>
2017-01-03 11:29:14 +0100

    Add flake8 as tox testenv as part of the testing process

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 
 import sys
-if sys.version_info < (3,0):
-    sys.exit('Sorry, Python < 3.0 is not supported')
-
 from setuptools import setup, find_packages
 import pyecore
 
+if sys.version_info < (3, 0):
+    sys.exit('Sorry, Python < 3.0 is not supported')
+
+
 setup(
-    name = "pyecore",
-    version = pyecore.__version__,
-    description = ("A Pythonic Implementation of the Eclipse Modeling "
-                   "Framework"),
-    long_description = open('README.rst').read(),
-    keywords = "model metamodel EMF Ecore",
-    url = "https://github.com/aranega/pyecore",
-    author = "Vincent Aranega",
-    author_email = "vincent.aranega@gmail.com",
+    name="pyecore",
+    version=pyecore.__version__,
+    description=("A Pythonic Implementation of the Eclipse Modeling "
+                 "Framework"),
+    long_description=open('README.rst').read(),
+    keywords="model metamodel EMF Ecore",
+    url="https://github.com/aranega/pyecore",
+    author="Vincent Aranega",
+    author_email="vincent.aranega@gmail.com",
 
-    packages = find_packages(exclude=['examples']),
-    package_data = {'': ['LICENSE',
-                       'README.rst']
-                   },
-    include_package_data = True,
-    install_requires = ['ordered-set', 'lxml'],
-    extras_require = {'testing': ['pytest'],},
+    packages=find_packages(exclude=['examples']),
+    package_data={'': ['LICENSE',
+                       'README.rst']},
+    include_package_data=True,
+    install_requires=['ordered-set', 'lxml'],
+    extras_require={'testing': ['pytest']},
 
-    license = 'BSD 3-Clause',
-    classifiers = [
+    license='BSD 3-Clause',
+    classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
