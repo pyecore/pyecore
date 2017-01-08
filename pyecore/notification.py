db5e315aa53fd27f22e3452326e443033751a240
Vincent Aranega <vincent.aranega@gmail.com>
2017-01-08 10:59:58 +0100

    Add first implementation of a basic notification system
    A new type 'Observer' had been introduced, which can observe an
    existing EObject. When it observes the eobject, it is notified each
    time a modification is performed on the observed
    element. Modifications are related to meta-property modifications as
    ADD, REMOVE...
    
    In order to make all the EObject observable, EObject now extends an
    Observable class. This class sketches the basic behavior for
    observable/observation.

---
+++ pyecore/notification.py
@@ -0,0 +1,70 @@
+
+
+class Observable(object):
+    def notify(self, notification):
+        notification.notifier = notification.notifier or self
+        for listener in self.listeners:
+            listener.notify(notification)
+
+
+def enum(enumName, *listValueNames):
+    """Clever implementation of an enum like in python
+
+    Shameless copy from: http://sametmax.com/faire-des-enums-en-python/
+    """
+    listValueNumbers = range(len(listValueNames))
+    dictAttrib = dict(zip(listValueNames, listValueNumbers))
+    dictReverse = dict(zip(listValueNumbers, listValueNames))
+    dictAttrib["dictReverse"] = dictReverse
+    mainType = type(enumName, (), dictAttrib)
+    return mainType
+
+
+Kind = enum('Kind',
+            'ADD',
+            'ADD_MANY',
+            'MOVE',
+            'REMOVE',
+            'REMOVE_MANY',
+            'SET',
+            'UNSET')
+
+
+class Notification(object):
+    def __init__(self, notifier=None, kind=None, old=None, new=None,
+                 feature=None):
+        self.notifier = notifier
+        self.kind = kind
+        self.old = old
+        self.new = new
+        self.feature = feature
+
+    def __repr__(self):
+        return ('[{0}] old={1} new={2} obj={3} #{4}'
+                .format(Kind.dictReverse[self.kind],
+                        self.old,
+                        self.new,
+                        self.notifier,
+                        self.feature))
+
+
+class Observer(object):
+    def __init__(self, observable=None, notify=None):
+        if observable:
+            observable.listeners.append(self)
+        if notify:
+            self.notify = notify
+
+    def observe(self, observable):
+        observable.listeners.append(self)
+
+    def notify(self, notification):
+        pass
+
+
+class PrintObserver(Observer):
+    def __init__(self, observable=None):
+        super().__init__(observable)
+
+    def notify(self, notification):
+        print(notification)
