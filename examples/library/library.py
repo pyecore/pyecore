8ebdc1c003fef1242e9cd3579f8600e6e170c9bf
Vincent Aranega <vincent.aranega@gmail.com>
2017-04-11 20:48:44 +0200

    Add new generated code for the 'library' example and tests
    The code uses the last generator with the improvements added by
    @moltob.

--- examples/library/library.py
+++ examples/library/library.py
@@ -19,6 +19,16 @@ class Employee(EObject, metaclass=MetaEClass):
     name = EAttribute(eType=EString)
     age = EAttribute(eType=EInt)
 
+    def __init__(self, name_=None, age_=None, **kwargs):
+        if kwargs:
+            raise AttributeError('unexpected arguments: {}'.format(kwargs))
+
+        super().__init__()
+        if name_ is not None:
+            self.name = name_
+        if age_ is not None:
+            self.age = age_
+
 
 class Library(EObject, metaclass=MetaEClass):
     name = EAttribute(eType=EString)
@@ -27,10 +37,36 @@ class Library(EObject, metaclass=MetaEClass):
     writers = EReference(upper=-1, containment=True)
     books = EReference(upper=-1, containment=True)
 
+    def __init__(self, name_=None, address_=None, employees_=None, writers_=None, books_=None, **kwargs):
+        if kwargs:
+            raise AttributeError('unexpected arguments: {}'.format(kwargs))
+
+        super().__init__()
+        if name_ is not None:
+            self.name = name_
+        if address_ is not None:
+            self.address = address_
+        if employees_:
+            self.employees.extend(employees_)
+        if writers_:
+            self.writers.extend(writers_)
+        if books_:
+            self.books.extend(books_)
+
 
 class Writer(EObject, metaclass=MetaEClass):
     name = EAttribute(eType=EString)
     books = EReference(upper=-1)
+
+    def __init__(self, name_=None, books_=None, **kwargs):
+        if kwargs:
+            raise AttributeError('unexpected arguments: {}'.format(kwargs))
+
+        super().__init__()
+        if name_ is not None:
+            self.name = name_
+        if books_:
+            self.books.extend(books_)
 
 
 class Book(EObject, metaclass=MetaEClass):
@@ -38,3 +74,17 @@ class Book(EObject, metaclass=MetaEClass):
     pages = EAttribute(eType=EInt)
     category = EAttribute(eType=BookCategory)
     authors = EReference(upper=-1)
+
+    def __init__(self, title_=None, pages_=None, category_=None, authors_=None, **kwargs):
+        if kwargs:
+            raise AttributeError('unexpected arguments: {}'.format(kwargs))
+
+        super().__init__()
+        if title_ is not None:
+            self.title = title_
+        if pages_ is not None:
+            self.pages = pages_
+        if category_ is not None:
+            self.category = category_
+        if authors_:
+            self.authors.extend(authors_)
