507fe8ffd57f160b1f5a427249078f505e662458
Mike Pagel <mike@mpagel.de>
2017-04-28 18:19:27 +0200

    added test for task run

--- tests/pygen/test_generator.py
+++ tests/pygen/test_generator.py
@@ -36,5 +36,14 @@ def test__generator__generate__tasks():
     ]
 
 
-def test__file_task():
-    element = EModelElement()
+@mock.patch.object(Task, 'ensure_folder')
+@mock.patch.object(Task, 'relative_path_for_element', return_value='file.ext')
+@mock.patch.object(Task, 'generate_file')
+def test__task__run(mock_generate_file, mock_relative_path_for_element, mock_ensure_folder):
+    task = Task()
+    task.run(mock.sentinel.ELEMENT, 'somefolder')
+
+    outfile = os.path.join('somefolder', 'file.ext')
+    mock_relative_path_for_element.assert_called_once_with(mock.sentinel.ELEMENT)
+    mock_ensure_folder.assert_called_once_with(outfile)
+    mock_generate_file.assert_called_once_with(mock.sentinel.ELEMENT, outfile)
