77383c33996f4868ce7c884cfbbb8805b501e04f
Mike Pagel <mike@mpagel.de>
2017-05-02 07:32:21 +0200

    base mechanics of pyecore's class generator

--- pygen/ecore.py
+++ pygen/ecore.py
@@ -1,11 +1,13 @@
 """Support for generation for models based on pyecore."""
+import os
+
+from pyecore.ecore import EPackage
+from pygen.jinja import JinjaTask, JinjaGenerator
 
 
-class ModelTypeMixin:
+class EcoreTask(JinjaTask):
     """
-    Implements the model filter by returning all elements of a certain element type.
-    
-    Use this mixin to add the model type iteration faility to another generator task class.
+    Base class for Jinja based generation of Pyecore models.
     
     Attributes:
         element_type: Ecore type to be searched in model and to be iterated over.
@@ -14,4 +16,47 @@ class ModelTypeMixin:
     element_type = None
 
     def filtered_elements(self, model):
+        """Return iterator based on `element_type`."""
         return (e for e in model.eAllContents() if isinstance(e, self.element_type))
+
+    @classmethod
+    def folder_path_for_package(cls, package: EPackage):
+        parent = package.eContainer()
+        if parent:
+            return os.path.join(cls.folder_path_for_package(parent), package.name)
+        return package.name
+
+
+class EcorePackageInitTask(EcoreTask):
+    """Generation of package init file from Ecore model with Jinja2."""
+
+    template_name = 'package.py'
+    element_type = EPackage
+
+    def relative_path_for_element(self, element: EPackage):
+        folder = self.folder_path_for_package(element)
+        path = os.path.join(folder, '__init__.py')
+        return path
+
+
+class EcorePackageModuleTask(EcoreTask):
+    """Generation of package model from Ecore model with Jinja2."""
+
+    template_name = 'module.py'
+    element_type = EPackage
+
+    def relative_path_for_element(self, element: EPackage):
+        folder = self.folder_path_for_package(element)
+        path = os.path.join(folder, '{}.py'.format(element.name))
+        return path
+
+
+class EcoreGenerator(JinjaGenerator):
+    """Generation of static ecore model classes."""
+
+    tasks = [
+        EcorePackageInitTask,
+        EcorePackageModuleTask,
+    ]
+
+    templates_path = 'templates'
--- tests/pygen/test_ecore.py
+++ tests/pygen/test_ecore.py
@@ -1,10 +1,12 @@
+import os
+
 import pytest
 
-from pyecore.ecore import EObject, EcoreUtils, EPackage, EClass, EEnum
-from pygen.ecore import ModelTypeMixin
+from pyecore.ecore import EPackage, EClass, EEnum
+from pygen.ecore import EcoreTask, EcorePackageInitTask, EcorePackageModuleTask
 
 
-def test__model_type_mixin__filtered_elements():
+def test__ecore_task__filtered_elements():
     # prepare test model:
     # -------------------
     # ThePackage
@@ -23,10 +25,30 @@ def test__model_type_mixin__filtered_elements():
     package.eClassifiers.append(class1)
     subpackage.eClassifiers.extend([class2, enum])
 
-    mixin = ModelTypeMixin()
+    mixin = EcoreTask()
 
     mixin.element_type = EClass
     assert set(mixin.filtered_elements(package)) == {class1, class2}
 
     mixin.element_type = EEnum
     assert set(mixin.filtered_elements(package)) == {enum}
+
+
+@pytest.fixture
+def package_in_hierarchy():
+    pkg1 = EPackage('pkg1')
+    pkg2 = EPackage('pkg2')
+    pkg3 = EPackage('pkg3')
+    pkg1.eSubpackages.append(pkg2)
+    pkg2.eSubpackages.append(pkg3)
+    return pkg3
+
+
+def test__ecore_package_init_task__path_for_element(package_in_hierarchy):
+    path = EcorePackageInitTask().relative_path_for_element(package_in_hierarchy)
+    assert path == os.path.join('pkg1', 'pkg2', 'pkg3', '__init__.py')
+
+
+def test__ecore_package_module_task__path_for_element(package_in_hierarchy):
+    path = EcorePackageModuleTask().relative_path_for_element(package_in_hierarchy)
+    assert path == os.path.join('pkg1', 'pkg2', 'pkg3', 'pkg3.py')
