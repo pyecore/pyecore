6793d8e12b250e10635e9ec996d8668556ba7aa7
Vincent Aranega <vincent.aranega@gmail.com>
2017-05-20 10:41:36 +0200

    Change version number location

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 import sys
 from setuptools import setup, find_packages
-import pyecore
 
 if sys.version_info < (3, 3):
     sys.exit('Sorry, Python < 3.3 is not supported')
@@ -13,15 +12,15 @@ if sys.version_info < (3, 4):
 
 
 setup(
-    name="pyecore",
-    version=pyecore.__version__,
-    description=("A Python(ic) Implementation of the Eclipse Modeling "
-                 "Framework (EMF/Ecore)"),
+    name='pyecore',
+    version='0.3.1.dev0',
+    description=('A Python(ic) Implementation of the Eclipse Modeling '
+                 'Framework (EMF/Ecore)'),
     long_description=open('README.rst').read(),
-    keywords="model metamodel EMF Ecore MDE",
-    url="https://github.com/pyecore/pyecore",
-    author="Vincent Aranega",
-    author_email="vincent.aranega@gmail.com",
+    keywords='model metamodel EMF Ecore MDE',
+    url='https://github.com/pyecore/pyecore',
+    author='Vincent Aranega',
+    author_email='vincent.aranega@gmail.com',
 
     packages=find_packages(exclude=['examples', 'tests']),
     package_data={'': ['LICENSE',
@@ -32,13 +31,13 @@ setup(
 
     license='BSD 3-Clause',
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3 :: Only",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries",
-        "License :: OSI Approved :: BSD License",
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
     ]
 )
