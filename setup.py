3268f28ebdd3841977a657d1ec75e0ce4692c637
Vincent Aranega <vincent.aranega@gmail.com>
2017-05-27 17:04:34 +0200

    Fix once and for all (I hope) the setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ from setuptools import setup, find_packages
 if sys.version_info < (3, 3):
     sys.exit('Sorry, Python < 3.3 is not supported')
 
+packages = ['pyecore',
+            'pyecore.resources']
+
 setup(
     name='pyecore',
     version='0.5.6-dev',
@@ -17,13 +20,13 @@ setup(
     author='Vincent Aranega',
     author_email='vincent.aranega@gmail.com',
 
-    packages=find_packages(exclude=['examples', 'tests']),
-    # data_files=[('', ['LICENSE', 'README.rst'])],
+    packages=packages,
+    package_data={'': ['README.rst', 'LICENSE', 'CHANGELOG.rst']},
+    include_package_data=True,
     install_requires=['enum34;python_version<"3.4"',
                       'ordered-set',
                       'lxml'],
     tests_require={'pytest'},
-
     license='BSD 3-Clause',
     classifiers=[
         'Development Status :: 4 - Beta',
