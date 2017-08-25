f92a960afe123362d95bc5238881fea6fc96d47b
Vincent Aranega <vincent.aranega@gmail.com>
2017-08-25 08:51:39 +0200

    Add first implementation of XMLTypes
    XMLTypes are generated from EMF XMLTypes.ecore. The generation had
    been made using pyecoregen and required some minor manual
    modifications. The actual modifications are:
     - addition of the missing import for 'EDataType' and 'EStringToStringMapEntry'
       in '__init__.py'
     - addition of 'from_string' and 'to_string' lambdas for required types
    
    There is still some Java types that need a proper translation:
     - 'javax.xml.datatype.XMLGregorianCalendar'
     - 'javax.xml.datatype.Duration'
     - 'javax.xml.namespace.QName'

--- pyecore/resources/__init__.py
+++ pyecore/resources/__init__.py
@@ -1,6 +1,7 @@
 from .resource import ResourceSet, Resource, URI, global_registry
 from . import xmi
 from .. import ecore as Ecore
+from .. import type as XMLType
 
 
 # Register basic resource factory
@@ -9,5 +10,6 @@ ResourceSet.resource_factory = {'xmi': lambda uri: xmi.XMIResource(uri),
                                 '*': lambda uri: xmi.XMIResource(uri)}
 
 global_registry[Ecore.nsURI] = Ecore
+global_registry[XMLType.nsURI] = XMLType
 
 __all__ = ['ResourceSet', 'Resource', 'URI', 'global_registry']
