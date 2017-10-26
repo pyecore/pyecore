ff69215d07c8152b1ecb49e85bd6e68641a1baa5
Vincent Aranega <vincent.aranega@gmail.com>
2017-10-26 10:59:49 +0200

    Remove Ecore metamodel from a Resource
    Each metamodel registered in a ResourceSet or the global_registry
    should not be part of a Resource. The fact that they are registered in
    a Resource implies that they are part of the same level than the
    Resource which is serialized. However, they are part of something
    "greater".

--- pyecore/resources/__init__.py
+++ pyecore/resources/__init__.py
@@ -9,8 +9,4 @@ ResourceSet.resource_factory = {'xmi': lambda uri: xmi.XMIResource(uri),
 
 global_registry[Ecore.nsURI] = Ecore
 
-ecore_resource = Resource(uri=Ecore.nsURI)
-ecore_resource.append(Ecore.eClass)
-
-
 __all__ = ['ResourceSet', 'Resource', 'URI', 'global_registry']
