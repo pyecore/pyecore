fb7cea5f7c9bd03736b2d4292d9f3ca89072e12f
Vincent Aranega <vincent.aranega@gmail.com>
2018-10-28 19:10:13 +0100

    Fix bad EAttribute for EClassifier
    The instanceTypeName EAttribute was misnamed for the static EAttribute
    for EClassifier.

--- pyecore/ecore.py
+++ pyecore/ecore.py
@@ -1016,7 +1016,7 @@ EClassifier.ePackage = EReference('ePackage', EPackage,
                                   eOpposite=EPackage.eClassifiers)
 EClassifier.eTypeParameters = EReference('eTypeParameters', ETypeParameter,
                                          upper=-1, containment=True)
-EClassifier.instanceClassName = EAttribute('instanceTypeName', EString)
+EClassifier.instanceTypeName = EAttribute('instanceTypeName', EString)
 EClassifier.instanceClass = EAttribute('instanceClass', EJavaClass)
 EClassifier.defaultValue = EAttribute('defaultValue', EJavaObject)
 EClassifier.instanceTypeName = EAttribute('instanceTypeName', EString,
