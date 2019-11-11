7c99c1c3da178b2469cd2e3350b11dc47bc41251
Björn Annighöfer <bjoern.annighoefer@ils.uni-stuttgart.de>
2019-11-11 17:36:17 +0100

    * fixed issue that breaks containment if I add an object from a containment feature of one parent to the same feature of another parent, i.e. the object was contained in both afterwards

--- pyecore/valuecontainer.py
+++ pyecore/valuecontainer.py
@@ -74,8 +74,10 @@ class PyEcoreValue(object):
                 resource.remove(value)
             prev_container = value._container
             prev_feature = value._containment_feature
-            if prev_feature != self.feature \
-                    and isinstance(prev_container, EObject):
+            #if prev_feature != self.feature \ #BA fix: feature is the same for objects being instances of the same class
+            if (prev_container != self.owner \
+                    or prev_feature != self.feature) \
+                    and isinstance(prev_container, EObject): #BA fix end.
                 prev_container.__dict__[prev_feature.name] \
                               .remove_or_unset(value)
             value._container = self.owner
