c7195dafacc0dc05b99fc318551ed0475105c90f
aranega <vincent.aranega@gmail.com>
2020-04-30 14:52:01 +0200

    Drop Python 3.4 support

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
