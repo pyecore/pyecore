3f109ce118fd9d738c259a9aa953f8f8fe48fe45
aranega <vincent.aranega@gmail.com>
2024-03-11 11:09:32 -0600

    Bump version: 0.15.1-dev → 0.15.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ packages = ['pyecore',
 
 setup(
     name='pyecore',
-    version='0.15.1-dev',
+    version='0.15.1',
     description=('A Python(ic) Implementation of the Eclipse Modeling '
                  'Framework (EMF/Ecore)'),
     long_description=open('README.rst').read(),
